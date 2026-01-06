ca7cfd781bc25b6bf3ecf54a738db523ef80fcff
anethsara959-beep <anethsara959@gmail.com>
2026-01-06 21:25:27 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+const BOT_NAME_FANCY = 'Qᴜᴇᴇɴ ꜱᴇʏᴀ xᴍᴅ🧸';
+
+const config = {
+  AUTO_VIEW_STATUS: 'true',
+  AUTO_LIKE_STATUS: 'true',
+  AUTO_RECORDING: 'false',
+  AUTO_LIKE_EMOJI: ['☘️','💗','🫂','🙈','🍁','🙃','🧸','😘','🏴‍☠️','👀','❤️‍🔥'],
+  PREFIX: '.',
+  MAX_RETRIES: 3,
+  GROUP_INVITE_LINK: 'https://chat.whatsapp.com/JHBUx8ILjM4KWi9581HRi4',
+  RCD_IMAGE_PATH: 'https://files.catbox.moe/oq9ey8.jpeg',
+  NEWSLETTER_JID: '120363424615222831@newsletter',
+  OTP_EXPIRY: 300000,
+  WORK_TYPE: 'public',
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '94778430626',
+  CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vb74ZB01iUxQUT4A8P2b',
+  BOT_NAME: 'Qᴜᴇᴇɴ ꜱᴇʏᴀ xᴍᴅ🧸',
+  BOT_VERSION: '1.0.0V',
+  OWNER_NAME: 'ᴘᴀᴍᴜᴅɪɴᴀ ʀᴀᴠɪʜᴀʀᴀ',
+  IMAGE_PATH: 'https://files.catbox.moe/oq9ey8.jpeg',
+  BOT_FOOTER: '> ρσωєя∂ ву Qᴜᴇᴇɴ ꜱᴇʏᴀ xᴍᴅ🧸 ',
+  BUTTON_IMAGES: { ALIVE: 'https://files.catbox.moe/oq9ey8.jpeg' }
